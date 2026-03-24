31c72ec6d3133d6bbd69d70b6c7debc3d783ee46
sarangapani12 <sarangapanir04@gmail.com>
2026-03-24 16:40:24 +0530

    added new retxt and changed settinggs.py

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -116,8 +116,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+import os
 
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = 'static/'
 
 # Default primary key field type
